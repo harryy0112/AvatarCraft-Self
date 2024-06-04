be9b124cbfc2e4da3d8120efba21efa562b0e019
Hardik Agarwal <agarwalhardik562@gmail.com>
2024-06-05 02:21:11 +0530

    updates

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "this-won't word unless you put your api URL";
+const baseUrl =
+  "https://0z2p883s10.execute-api.eu-west-3.amazonaws.com/dev/transform";
 
 function transform(data) {
   return axios.post(baseUrl, data);
